198fc0b2ae228a9846124c47302c24cb8f4b7166
Laura Neves <lauracmneves@gmail.com>
2022-04-28 23:27:32 -0300

    Feat: /participants

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -2,27 +2,43 @@ import express, {json} from "express";
 import cors from "cors";
 import chalk from "chalk";
 import { MongoClient } from "mongodb";
+import joi from 'joi';
 import dotenv from "dotenv";
-
 dotenv.config();
 
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect(() =>{
+    db = mongoClient.db(process.env.BANCO_MONGO)
+})
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-let db;
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+app.post("/participants", async (req,res) =>{
+    const {name} = req.body
+    const schema = joi.string()
+    const {error} = schema.validate(name)
 
-// mongoClient.connect().then(() => {
-// 	db = mongoClient.db("meu_lindo_projeto");
-// });
-
+    try{
+        if(error){
+            return res.sendStatus(422);
+        }
+        const participant = await db.collection("participants").insertOne({name, lastStatus: Date.now()})
+        res.send(participant).status(201);
+    }catch(e){
+        console.error(e)
+        res.sendStatus(500);
+    }
+});
 
 app.get("/",(req,res)=>{
     res.send("Deu bom!")
 })
 
-app.listen(5000,()=>{
-console.log(chalk.green.bold("Server is running on: http://localhost:5000"))
+app.listen(process.env.PORTA,()=>{
+console.log(chalk.green.bold(`Server is running on: http://localhost:${process.env.PORTA}`))
 })
